ec6e128ff4616ea3a88623bc031feaf36b0f92b0
Boopesh <tboopeshkumar@gmail.com>
2021-05-08 18:28:05 +0400

    style conflict fix

--- MicroFrontEnd/container/src/App.js
+++ MicroFrontEnd/container/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi From Container!!!</h1>
-      <MarketingApp />;
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />;
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
