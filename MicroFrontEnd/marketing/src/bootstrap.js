de6c51ddc3e7ca61bb0bf6e4730555126bf1f65b
Boopesh <tboopeshkumar@gmail.com>
2021-05-11 10:59:30 +0400

    route handling in microfrontend

--- MicroFrontEnd/marketing/src/bootstrap.js
+++ MicroFrontEnd/marketing/src/bootstrap.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 // Mount function to start the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory ?? createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      console.log("on parent navigate", pathname, nextPathname);
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation, call mount immediately
@@ -12,7 +26,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
